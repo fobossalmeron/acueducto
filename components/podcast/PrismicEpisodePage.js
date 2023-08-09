379435c2d613c9f509c27feab29e8186bcc30747
Rocio Arroyo <rocionahir.arroyo@gmail.com>
2023-08-09 11:05:56 -0300

    condicional para ep de prismic y mds

---
+++ components/podcast/PrismicEpisodePage.js
@@ -0,0 +1,339 @@
+import React from "react";
+import styled from "styled-components";
+import { H1, Li } from "components/shared/Dangerously";
+import { Fade } from "react-awesome-reveal";
+import EpisodePreview from "components/podcast/EpisodePreview";
+import Logo from "public/assets/img/layout/logo.svg";
+import EpisodeNumber from "./EpisodeNumber";
+import Link from "next/link";
+import BorderLink from "components/shared/BorderedLink";
+import CenteredSection, {
+  Content,
+  Insights,
+  Transcript,
+} from "components/shared/CenteredSection";
+import ShareRouter from "./ShareRouter";
+import YouTubePlayer from "react-player/youtube";
+
+const EpisodePage = ({
+  uid,
+  data,
+}) => {
+  const episode = data.introduction[0].episode;
+  const category = data.introduction[0].category;
+  const title = data.introduction[0].title[0].text;
+  const guest = data.introduction[0].guest;
+  const business = data.introduction[0].business;
+  const date = data.introduction[0].date;
+  const description = data.introduction[0].description[0].text;
+  const spotify = data.introduction[0].spotify;
+  const apple = data.introduction[0].apple;
+  const google = data.introduction[0].google;
+  const youtube = data.introduction[0].youtube;
+  const youtubeImage = data.images[0].youtube;
+  const insights = data.introduction[0].insights;
+  const content = data.body;
+
+  const embedYoutube = youtube && youtube.replace("watch?v=", "embed/");
+  return (
+    <>
+      <CenteredSection
+        customBackground={"/assets/img/layout/backOld.svg"}
+        id="cuandoelriosuena"
+      >
+        <Fade triggerOnce>
+          <IntroLogo>
+            <Link href="/podcast" passHref>
+              <a>
+                <THoverable>cuando el río suena</THoverable>
+                <span>
+                  por <Logo />
+                </span>
+              </a>
+            </Link>
+          </IntroLogo>
+        </Fade>
+        <Fade triggerOnce>
+          <>
+            <EpisodeNumberStyled>
+              <EpisodeNumber episode={episode} />
+            </EpisodeNumberStyled>
+            <H1>{title.charAt(0).toLowerCase() + title.slice(1)}</H1>
+            {youtube && (
+              <Center>
+                <VideoContainer>
+                  <Video>
+                    <YouTubePlayer
+                      className="react-player"
+                      light={youtubeImage}
+                      url={embedYoutube}
+                      controls={true}
+                      width="100%"
+                      height="100%"
+                    />
+                  </Video>
+                </VideoContainer>
+              </Center>
+            )}
+          </>
+        </Fade>
+        <Fade triggerOnce>
+          <EpisodePreview
+            hideImageMobile
+            title={title}
+            guest={guest}
+            business={business}
+            slug={uid}
+            spotify={spotify}
+            apple={apple}
+            google={google}
+            youtube={youtube}
+            episode={episode}
+            description={description}
+            date={date}
+            category={category}
+            longFormat
+          />
+        </Fade>
+        <Fade triggerOnce>
+          {spotify && insights && (
+            <Content as={Insights}>
+              <ContentType insights>Insights</ContentType>
+              <p>
+                Si solo tienes un minuto, lo más importante que pueden aprender
+                operadores, inversionistas y fundadores de {business} es lo
+                siguiente:
+              </p>
+              <Transcript as={"div"}>
+                {insights && (
+                  <ul>
+                    {insights.map((insight, i) => (
+                      <Li key={"insight" + i} style={{ fontWeight: 200 }}>{insight.text}</Li>
+                    ))}
+                  </ul>
+                )}
+              </Transcript>
+            </Content>
+          )}
+        </Fade>
+        <Fade>
+          {spotify && (
+            <Content>
+              {content && <ContentType>Transcript</ContentType>}
+                {content.map((dialog, i) => (
+                  <PrismicTranscript 
+                    key={"dialog" + i}
+                    className={dialog.type}
+                  >
+                    {dialog.text.includes(':') 
+                      ? dialog.text.split(':')[0] + ':' + dialog.text.split(':')[1]
+                      : dialog.text 
+                    }
+                  </PrismicTranscript>
+                )) }
+            </Content>
+          )}
+        </Fade>
+        <Fade triggerOnce>
+          <CenteredDiv>
+            {content && spotify && (
+              <>
+                <RouterSpace>
+                  Si crees que a alguien le seria útil este contenido,
+                  compártelo con esa persona.
+                </RouterSpace>
+                <ShareRouter
+                  shareUrl={`https://acueducto.studio/podcast/${uid}`}
+                />
+              </>
+            )}
+          </CenteredDiv>
+        </Fade>
+        <NextEp>
+          <p>Escucha otro episodio</p>
+          {/* <EpisodePreview {...next} simplest /> */}
+        </NextEp>
+      </CenteredSection>
+    </>
+  );
+};
+
+export default React.memo(EpisodePage);
+
+const Center = styled.div`
+  display: flex;
+  justify-content: center;
+`;
+
+const VideoContainer = styled.div`
+  max-width: 800px;
+  width: 100%;
+  height: auto;
+  margin-top: 8%;
+  @media (max-width: 620px) {
+    margin-top: 10%;
+  }
+`;
+
+const Video = styled.div`
+  position: relative;
+  padding-top: 56.25%;
+  border-radius: 30px;
+  overflow: hidden;
+
+  .react-player {
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+`;
+
+const NextEp = styled.div`
+  margin-top: 10%;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: ${(props) => props.theme.colors.foreground_low};
+  p {
+    text-align: center;
+  }
+  @media (max-width: 620px) {
+    margin-top: 20%;
+  }
+`;
+
+const THoverable = styled.b`
+  font-weight: 300;
+  ${BorderLink({ showLink: false })}
+`;
+
+const CenteredDiv = styled.div`
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  flex-direction: column;
+`;
+
+const PrismicTranscript = styled(Transcript)`
+  font-size: 1.7rem;
+  line-height: 25px;
+  margin-bottom: 2rem;
+  color: rgb(79, 79, 79);
+
+  heading2 {
+    font-size: 2.6rem;
+    font-weight: 300;
+    color: rgb(24, 32, 36);
+    line-height: 120%;
+    text-align: left;
+    margin-bottom: 1.1rem;
+    margin-top: 4rem;
+  }
+`;
+
+const ContentType = styled.span`
+  font-weight: 300;
+  font-weight: ${(p) => (p.insights ? 600 : 300)};
+  text-transform: uppercase;
+  text-align: center;
+  letter-spacing: 6px;
+  color: ${(p) => (p.insights ? "#4DA465" : p.theme.colors.background)};
+  margin-bottom: 2rem;
+  font-size: 1.5rem;
+  width: 100%;
+  display: block;
+  transform: translateY(-100%);
+  @media (max-width: 900px) {
+    margin-top: 10px;
+  }
+  @media (max-width: 650px) {
+    margin-top: 30px;
+    margin-bottom: 1rem;
+  }
+  @media (max-width: 500px) {
+    font-size: 1.3rem;
+    margin-top: 40px;
+  }
+`;
+
+const RouterSpace = styled.div`
+  padding-top: 20%;
+  margin-top: 20px;
+  text-align: center;
+  max-width: 450px;
+  color: ${(props) => props.theme.colors.foreground_low};
+  @media (max-width: 800px) {
+    max-width: 400px;
+    padding-top: 10%;
+  }
+  @media (max-width: 500px) {
+    padding-top: 6%;
+  }
+`;
+
+const EpisodeNumberStyled = styled.div`
+  align-items: center;
+  justify-content: center;
+  display: flex;
+  @media (max-width: 1000px) {
+    transform: scale(0.9);
+  }
+  @media (max-width: 800px) {
+    transform: scale(0.8);
+  }
+`;
+
+const IntroLogo = styled.p`
+  line-height: 100%;
+  font-size: 3.5rem;
+  font-weight: 500;
+  margin-bottom: 3px;
+  color: ${(props) => props.theme.colors.foreground};
+  text-align: center;
+  margin-bottom: 15%;
+  text-decoration: none;
+  padding-top: 120px;
+  a {
+    text-decoration: none;
+  }
+  span {
+    display: block;
+    font-size: 2rem;
+    font-weight: 100;
+    color: ${(props) => props.theme.colors.accent};
+    svg {
+      max-width: 110px;
+      * {
+        fill: ${(props) => props.theme.colors.accent};
+      }
+    }
+  }
+  @media (max-width: 1300px) {
+    padding-top: 50px;
+  }
+  @media (max-width: 1250px) {
+    font-size: 3rem;
+  }
+  @media (max-width: 950px) {
+    font-size: 2.5rem;
+    span {
+      margin-top: 5px;
+      font-size: 1.5rem;
+      svg {
+        max-width: 90px;
+      }
+    }
+  }
+  @media (max-width: 800px) {
+    font-size: 2rem;
+    padding-top: 35px;
+    span {
+      margin-top: 0px;
+      font-size: 1.3rem;
+      svg {
+        max-width: 70px;
+      }
+    }
+  }
+`;
--- pages/podcast/[slug].tsx
+++ pages/podcast/[slug].tsx
@@ -1,5 +1,4 @@
-import { useEffect, useState } from "react";
-import { GetStaticProps } from "next";
+import { useEffect } from "react";
 import EpisodeProps from "utils/types/EpisodeProps";
 import markdownToHtml from "utils/markdownToHtml";
 import {
@@ -9,38 +8,77 @@ import {
 } from "utils/podcastApi";
 import Head from "components/layout/Head";
 import EpisodePage from "components/podcast/EpisodePage";
+import PrismicEpisodePage from "components/podcast/PrismicEpisodePage";
 import PageClipper from "components/layout/PageClipper";
 import ResourceFooter from "components/shared/footers/ResourceFooter";
 import type { GetStaticPropsContext } from 'next';
 import { createClient } from '../../prismicio';
-import * as prismic from '@prismicio/client'
 
-export default function Episodio({ locale, setTitle, episode, numberOfE }) {
+export default function Episodio({ locale, setTitle, episode, numberOfE, prismicEpisode, slugMatchesPrismic }) {
   useEffect(() => {
     setTitle("Podcast");
   }, [locale]);
 
+  if(slugMatchesPrismic){
+    const episodeNumber = slugMatchesPrismic.data.introduction[0].episode;
+    const category = slugMatchesPrismic.data.introduction[0].category;
+    const title = slugMatchesPrismic.data.introduction[0].title[0].text;
+    const guest = slugMatchesPrismic.data.introduction[0].guest;
+    const business = slugMatchesPrismic.data.introduction[0].business;
+    const date = slugMatchesPrismic.data.introduction[0].date;
+    const description = slugMatchesPrismic.data.introduction[0].description[0].text;
+    const spotify = slugMatchesPrismic.data.introduction[0].spotify;
+    const apple = slugMatchesPrismic.data.introduction[0].apple;
+    const google = slugMatchesPrismic.data.introduction[0].google;
+    const youtube = slugMatchesPrismic.data.introduction[0].youtube;
+    const gif = slugMatchesPrismic.data.images[0].gif;
+  }
+
   return (
     <PageClipper>
-      <Head
-        title={episode.title + " | " + episode.guest + ", " + episode.business}
-        description={episode.description}
-        headerTitle="Episodio"
-        es_canonical={`https://acueducto.studio/podcast/${episode.slug}`}
-        image={{
-          fileName:
-            episode.episode >= 63
-              ? `og_image_e${episode.episode}.gif`
-              : `og_image_e${episode.episode}.png`,
-          alt: episode.title + " | " + episode.guest + ", " + episode.business,
-        }}
-      ></Head>
-      <EpisodePage {...episode} slug={episode.slug} />
-      <ResourceFooter
-        shadow
-        identify={episode.slug}
-        podcastEpisodes={numberOfE}
-      />
+      {!slugMatchesPrismic &&
+        <>
+          <Head
+            title={episode.title + " | " + episode.guest + ", " + episode.business}
+            description={episode.description}
+            headerTitle="Episodio"
+            es_canonical={`https://acueducto.studio/podcast/${episode.slug}`}
+            image={{
+              fileName:
+                episode.episode >= 63
+                  ? `og_image_e${episode.episode}.gif`
+                  : `og_image_e${episode.episode}.png`,
+                alt: episode.title + " | " + episode.guest + ", " + episode.business,
+            }}
+          ></Head>
+          <EpisodePage {...episode} slug={episode.slug} />
+          <ResourceFooter
+            shadow
+            identify={episode.slug}
+            podcastEpisodes={numberOfE}
+          />
+        </>
+      }
+      {slugMatchesPrismic && 
+        <>
+          <Head
+            title={ slugMatchesPrismic.data.introduction[0].title[0].text + " | " + slugMatchesPrismic.data.introduction[0].guest + ", " + slugMatchesPrismic.data.introduction[0].business}
+            description={slugMatchesPrismic.data.introduction[0].description[0].text}
+            headerTitle="Episodio"
+            es_canonical={`https://acueducto.studio/podcast/${slugMatchesPrismic.uid}`}
+            image={{
+              fileName: slugMatchesPrismic.data.images[0].gif,
+              alt: slugMatchesPrismic.data.introduction[0].title[0].text + " | " + slugMatchesPrismic.data.introduction[0].guest + ", " + slugMatchesPrismic.data.introduction[0].business,
+            }}
+          ></Head>
+          <PrismicEpisodePage {...slugMatchesPrismic} slug={slugMatchesPrismic.uid} />
+          {/* <ResourceFooter
+            shadow
+            identify={prismicEpisode.slug[0]}
+            podcastEpisodes={numberOfE}
+          />  */}
+        </>
+      }
     </PageClipper>
   );
 }
@@ -49,94 +87,97 @@ export async function getStaticProps({
   params,
   previewData
 }: GetStaticPropsContext) {
-  if (!params.slug){
-    const episode: EpisodeProps = getEpisodeBySlug(params.slug, [
-    "title",
-    "guest",
-    "date",
-    "insights",
-    "business",
-    "category",
-    "description",
-    "episode",
-    "slug",
-    "spotify",
-    "apple",
-    "google",
-    "youtube",
-    "content",
-  ]);
-  const next: EpisodeProps = getEpisodeBySlug(
-    getNextEpisodeSlug(episode.episode),
-    [
-      "title",
-      "guest",
-      "date",
-      "business",
-      "category",
-      "description",
-      "episode",
-      "slug",
-      "spotify",
-      "apple",
-      "google",
-      "youtube",
-    ]
-  );
-
-  const content = await markdownToHtml(episode.content || "");
-
-  //For podcast episode number in footer
-  const episodes = getAllEpisodes(["slug"]);
-  const numberOfE = Object.keys(episodes).length + 1;
-  
-  if (!episode) {
-    console.log(episode.slug,'slug')
-    return {
-      notFound: true,
-    };
-  }
-  return {
-    props: {
-      numberOfE: numberOfE,
-      episode: {
-        ...episode,
-        content,
-        next,
-      },
-      nextEpisode: {
-        ...next,
-      }
-    },
-  };
-  }
-  else {
-        //CMS Prismic
+    //CMS Prismic
     const client = createClient({ previewData });
-    const prismicEpisode = await client.getByUID('episode', `no-vivas-de-tus-usuarios-construye-tu-futuro-junto-con-ellos`)
+    const prismicEpisode = await client.getAllByType("episode");
 
-    console.log('funciono :)')
+    const slugMatchesPrismic = prismicEpisode.find(objeto => objeto.uid === params.slug);
 
-  return {
-    props: {
-      prismicEpisode: {
-        ...prismicEpisode,
+    if(!slugMatchesPrismic) {
+      const episode: EpisodeProps = getEpisodeBySlug(params.slug, [
+        "title",
+        "guest",
+        "date",
+        "insights",
+        "business",
+        "category",
+        "description",
+        "episode",
+        "slug",
+        "spotify",
+        "apple",
+        "google",
+        "youtube",
+        "content",
+      ]);
+      const next: EpisodeProps = getEpisodeBySlug(
+        getNextEpisodeSlug(episode.episode),
+        [
+          "title",
+          "guest",
+          "date",
+          "business",
+          "category",
+          "description",
+          "episode",
+          "slug",
+          "spotify",
+          "apple",
+          "google",
+          "youtube",
+        ]
+      );
+    
+      const content = await markdownToHtml(episode.content || "");
+    
+      //For podcast episode number in footer
+      const episodes = getAllEpisodes(["slug"]);
+      const numberOfE = Object.keys(episodes).length + 1;
+      
+      if (!episode) {
+        return {
+          notFound: true,
+        };
       }
-    },
-  };
-  }
+
+      return {
+        props: {
+          numberOfE: numberOfE,
+          episode: {
+            ...episode,
+            content,
+            next,
+          },
+          nextEpisode: {
+            ...next,
+          },
+          slugMatchesPrismic: slugMatchesPrismic || null,
+        },
+      };
+    } else {
+      return {
+        props: {
+          slugMatchesPrismic: slugMatchesPrismic || null,
+        },
+      };
+    }
 };
 
 export async function getStaticPaths() {
   const episodes = getAllEpisodes(["slug"]);
-    return {
-      paths: episodes.map((episode: EpisodeProps) => {
-        return {
-          params: {
-            slug: episode.slug,
-          },
-        };
-      }),
-      fallback: false,
-  } 
+
+  const client = createClient({ previewData: null });
+  const prismicEpisodes = await client.getAllByType("episode");
+  const prismicSlugs = prismicEpisodes.map((result) => result.uid);
+
+  const allSlugs = [...episodes.map((episode) => episode.slug), ...prismicSlugs];
+
+  return {
+    paths: allSlugs.map((slug) => ({
+      params: {
+        slug: slug,
+      },
+    })),
+    fallback: false,
+  };
 }
