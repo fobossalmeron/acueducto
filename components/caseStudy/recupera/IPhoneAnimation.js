4c3e9d7507b12b39544c3d6d800d582355fd4d17
Rocio Arroyo <rocionahir.arroyo@gmail.com>
2024-02-27 21:18:09 -0300

    removed unnecessary elements in IphoneAnimation and ScrollCardAnimation components

--- components/caseStudy/recupera/IPhoneAnimation.js
+++ components/caseStudy/recupera/IPhoneAnimation.js
@@ -7,51 +7,30 @@ import UIMobile2 from "public/assets/img/casestudies/recupera/UIMobile2.png";
 import UIMobile3 from "public/assets/img/casestudies/recupera/UIMobile3.png";
 import { useState } from "react";
 import Image from "next/legacy/image";
+import { InView } from "react-intersection-observer";
 
 const IPhoneAnimation = ({ isMobile }) => {
   const [scrollPosition, setScrollPosition] = useState(0);
-  const [animationInProgress, setAnimationInProgress] = useState(false);
-  // Rocío: getElementById o el querySelector funcionan por fuera de React, a veces fallarán porque
-  // react actualiza el DOM y no hay control. Hay que usar el hook de useRef que está debajo (a menos
-  // que esté en otro componente lejísimos, como que quisieramos cambiarle el color al logo, ahí pues
-  // si es considerablemente menos pedo hacerlo con js pelón).
+  const [animation, setAnimation] = useState(true);
+
   const animationContainer = useRef();
   let scrollY = 0;
-
-  useEffect(() => {
-    window.addEventListener("scroll", handleScroll);
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
 
   const handleScroll = () => {
     scrollY =
       animationContainer.current?.getBoundingClientRect().top +
       (isMobile ? +-300 : 0);
-    //Rocío. Te dejé comentado el console.log pero préndelo para que veas la cantidad de calls que hace esta función
-    //aún cuando no estamos viendo la animación. La explicación en el texto de abajo.
-    //console.log(scrollY);
-
-    if (!animationInProgress) {
-      setScrollPosition(scrollY);
-      setAnimationInProgress(true);
-
-      //Rocío: El animation in progress de arriba no está haciendo nada y estamos observando el scroll
-      //durante toda la página. Esto puede traer problemas de performance. Hay que usar algo
-      //como el Intersection Observer para que solo se active el EventListener cuando esté en el viewport
-      //este elemento. Está bien quitarlo al desmontar el componente pero igual está activo toda la página
-      //Me parece que ya importamos un par de librerías para usar el Intersection Observer fácilmente
-
-      //Rocío: Me di cuenta que también estás añadiendo más EventListeners en el componente de
-      //ScrollCardAnimation.js, esto añade al problema. Con mi solución no habría problema de que lo hagas
-      //en dos componentes separados si solo se activa cuando pasas por ahí.
-
-      setTimeout(() => {
-        setAnimationInProgress(false);
-      }, 2000);
-    }
+    setScrollPosition(scrollY);
   };
+
+  useEffect(() => {
+    if (animation) {
+      window.addEventListener("scroll", handleScroll);
+      return () => {
+        window.removeEventListener("scroll", handleScroll);
+      };
+    }
+  }, [animation]);
 
   const limitEffect = (speed) => {
     if (scrollPosition * speed > 20) {
@@ -78,34 +57,38 @@ const IPhoneAnimation = ({ isMobile }) => {
       bottom: "-20px",
     };
   };
-  //Rocio: Igual acá abajo puse alt's para todas las imágenes. Siempre hay que poner cosas contextuales
-  //porque nos ayuda en SEO y para lectores de pantalla. Poner "Imagen 1" no es útil.
   return (
-    <MobilePicture id="ScrollIphoneAnimation" ref={animationContainer}>
-      <div className="mobileBackground">
-        <Picture src={MobileBackground} alt="Recupera App" withWrapper />
-      </div>
-      <ContainerPictures
-        style={!isMobile ? getAnimationStyle() : getAnimationStyleMobile()}
-      >
-        <div className={`picture1`}>
-          <Image src={UIMobile1} alt="Saldo a favor" withWrapper />
+    <InView
+      as="div"
+      onChange={() => {
+        setAnimation(!animation);
+      }}
+    >
+      <MobilePicture id="ScrollIphoneAnimation" ref={animationContainer}>
+        <div className="mobileBackground">
+          <Picture src={MobileBackground} alt="Recupera App" withWrapper />
         </div>
-        <div className={`picture2`}>
-          <Image src={UIMobile2} alt="Factura deducible" withWrapper />
-        </div>
-        <div className={`picture3`}>
-          <Image src={UIMobile3} alt="Requisitos para deducir" withWrapper />
-        </div>
-      </ContainerPictures>
-    </MobilePicture>
+        <ContainerPictures
+          style={!isMobile ? getAnimationStyle() : getAnimationStyleMobile()}
+        >
+          <div className={`picture1`}>
+            <Image src={UIMobile1} alt="Saldo a favor" withWrapper />
+          </div>
+          <div className={`picture2`}>
+            <Image src={UIMobile2} alt="Factura deducible" withWrapper />
+          </div>
+          <div className={`picture3`}>
+            <Image src={UIMobile3} alt="Requisitos para deducir" withWrapper />
+          </div>
+        </ContainerPictures>
+      </MobilePicture>
+    </InView>
   );
 };
 
 export default IPhoneAnimation;
 
 const MobilePicture = styled.div`
-  width: 100%;
   position: relative;
 
   .mobileBackground {
--- components/caseStudy/recupera/ScrollCardAnimation.js
+++ components/caseStudy/recupera/ScrollCardAnimation.js
@@ -13,43 +13,22 @@ import styled from "styled-components";
 import { Fade } from "react-awesome-reveal";
 import { Element } from "react-scroll";
 import { useSpring, animated } from "@react-spring/web";
-
-/* 
-Rocío: refactoricé unas cosillas porque cambió el layout pero este componente está muy sucio.
-Tenemos que ser más cuidadosos, hay una imagen que no se está importando.
-No se utiliza el componente de imágenes de next que se encarga de optimizarlas a distintos
-tamaños de imagen, hay estilos duplicados, variables que no se usan, cosas comentadas, etc. 
-*/
+import { InView } from "react-intersection-observer";
 
 const ScrollCardAnimation = ({ isMobile }) => {
   const [moveAmount, setMoveAmount] = useState(0);
+  const [animation, setAnimation] = useState(true);
   let scrollY = 0;
 
   const handleScrollAnimation = () => {
-
     if (isMobile) {
       scrollY = window.scrollY;
     } else {
       scrollY = -window.scrollY;
     }
 
-    const progress = (scrollY - 0) / (window.innerHeight - 0) || 0;
-    const amount = setMoveAmount(progress * 7); // Ajustar este multiplicador según la velocidad de la animación mobile
-
-    const card1 = document.getElementById("card1");
-    if (card1) {
-      card1.style.transform = `translateX(${moveAmount}em)`;
-    }
-
-    const card2 = document.getElementById("card2");
-    if (card2) {
-      card2.style.transform = `translateX(${-moveAmount}em)`;
-    }
-
-    const card3 = document.getElementById("card3");
-    if (card3) {
-      card3.style.transform = `translateX(${moveAmount}em)`;
-    }
+    const progress = scrollY / window.innerHeight || 0;
+    setMoveAmount(progress * 7); // Ajustar este multiplicador según la velocidad de la animación mobile
   };
 
   let card1Animation = useSpring({
@@ -63,131 +42,173 @@ const ScrollCardAnimation = ({ isMobile }) => {
   });
 
   useEffect(() => {
-    window.addEventListener("scroll", handleScrollAnimation);
-    return () => {
-      window.removeEventListener("scroll", handleScrollAnimation);
-    };
-  }, [isMobile]);
+    if (animation) {
+      window.addEventListener("scroll", handleScrollAnimation);
+      return () => {
+        window.removeEventListener("scroll", handleScrollAnimation);
+      };
+    }
+  }, [isMobile, animation]);
 
   return (
     <Fade delay={300} triggerOnce>
-      {!isMobile ? (
-        <PicturesContainer>
-          <Element name="card1" className="scroll-element">
-            <animated.div style={card1Animation}>
-              <FirstRow id="card1">
-                <img
-                  src="/assets/img/casestudies/recupera/screenshot1.png"
-                  alt="Screenshots"
-                />
-                <img
-                  src="/assets/img/casestudies/recupera/screenshot4.png"
-                  alt="Screenshots"
-                />
-                <img
-                  src="/assets/img/casestudies/recupera/screenshot2.png"
-                  alt="Screenshots"
-                />
-                <img
-                  src="/assets/img/casestudies/recupera/screenshot3.png"
-                  alt="Screenshots"
-                />
-                <img
-                  src="/assets/img/casestudies/recupera/screenshot5.png"
-                  alt="Screenshots"
-                />
-              </FirstRow>
-            </animated.div>
-          </Element>
-          <Element name="card2" className="scroll-element">
-            <animated.div style={card2Animation}>
-              <SecondRow id="card2">
-                <img
-                  src="/assets/img/casestudies/recupera/screenshot6.png"
-                  alt="Screenshots"
-                />
-                <img
-                  src="/assets/img/casestudies/recupera/screenshot7.png"
-                  alt="Screenshots"
-                />
-                <img
-                  src="/assets/img/casestudies/recupera/screenshot8.png"
-                  alt="Screenshots"
-                />
-                <img
-                  src="/assets/img/casestudies/recupera/screenshot9.png"
-                  alt="Screenshots"
-                />
-              </SecondRow>
-            </animated.div>
-          </Element>
-        </PicturesContainer>
-      ) : (
-        <PicturesContainerMobile>
-          <Element name="card1" className="scroll-element">
-            <animated.div style={card1Animation}>
-              <FirstRowMobile id="card1">
-                <Picture src={Screenshot1} alt="Screenshots" withWrapper/>
-                <Picture src={Screenshot2} alt="Screenshots" withWrapper/>
-                <Picture src={Screenshot3} alt="Screenshots" withWrapper/>
-              </FirstRowMobile>
-            </animated.div>
-          </Element>
-          <Element name="card1" className="scroll-element">
-            <animated.div style={card2Animation}>
-              <SecondRowMobile id="card2">
-                <Picture src={Screenshot3} alt="Screenshots" withWrapper />
-                <Picture src={Screenshot7} alt="Screenshots" withWrapper />
-                <Picture src={Screenshot8} alt="Screenshots" withWrapper />
-                <Picture src={Screenshot1} alt="Screenshots" withWrapper />
-              </SecondRowMobile>
-            </animated.div>
-          </Element>
-          <Element name="card3" className="scroll-element">
-            <animated.div style={card1Animation}>
-              <ThirdRowMobile id="card3">
-                <Picture src={Screenshot6} alt="Screenshots" withWrapper />
-                <Picture src={Screenshot9} alt="Screenshots" withWrapper />
-                <Picture src={Screenshot5} alt="Screenshots" withWrapper />
-              </ThirdRowMobile>
-            </animated.div>
-          </Element>
-        </PicturesContainerMobile>
-      )}
+      <InView as="div" onChange={() => setAnimation(!animation)}>
+        {!isMobile ? (
+          <PicturesContainerDesktop>
+            <Element className="scroll-element">
+              <animated.div style={card1Animation}>
+                <RowDesktop>
+                  <Picture
+                    src={Screenshot1}
+                    alt="Opciones de la app de Recupera"
+                    withWrapper
+                  />
+                  <Picture
+                    src={Screenshot4}
+                    alt="Presentación de la app de Recupera"
+                    withWrapper
+                  />
+                  <Picture
+                    src={Screenshot2}
+                    alt="Homepage de la web de Recupera"
+                    withWrapper
+                  />
+                  <Picture
+                    src={Screenshot3}
+                    alt="Opciones de la app de Recupera"
+                    withWrapper
+                  />
+                  <Picture
+                    src={Screenshot5}
+                    alt="Mensaje de error 404 de la web de Recupera"
+                    withWrapper
+                  />
+                </RowDesktop>
+              </animated.div>
+            </Element>
+            <Element className="scroll-element">
+              <animated.div style={card2Animation}>
+                <RowDesktop>
+                  <Picture
+                    src={Screenshot6}
+                    alt="Footer de la web de Recupera"
+                    withWrapper
+                  />
+                  <Picture
+                    src={Screenshot7}
+                    alt="Presentación de la versión beta de la web de Recupera"
+                    withWrapper
+                  />
+                  <Picture
+                    src={Screenshot8}
+                    alt="Función de la app de Recupera"
+                    withWrapper
+                  />
+                  <Picture
+                    src={Screenshot9}
+                    alt="Mensaje de éxito de la web de Recupera"
+                    withWrapper
+                  />
+                </RowDesktop>
+              </animated.div>
+            </Element>
+          </PicturesContainerDesktop>
+        ) : (
+          <PicturesContainerMobile>
+            <Element className="scroll-element">
+              <animated.div style={card1Animation}>
+                <FirstRowMobile>
+                  <Picture
+                    src={Screenshot1}
+                    alt="Opciones de la app de Recupera"
+                    withWrapper
+                  />
+                  <Picture
+                    src={Screenshot2}
+                    alt="Homepage de la web de Recupera"
+                    withWrapper
+                  />
+                  <Picture
+                    src={Screenshot3}
+                    alt="Opciones de la app de Recupera"
+                    withWrapper
+                  />
+                </FirstRowMobile>
+              </animated.div>
+            </Element>
+            <Element className="scroll-element">
+              <animated.div style={card2Animation}>
+                <SecondRowMobile>
+                  <Picture
+                    src={Screenshot3}
+                    alt="Opciones de la app de Recupera"
+                    withWrapper
+                  />
+                  <Picture
+                    src={Screenshot7}
+                    alt="Presentación de la versión beta de la web de Recupera"
+                    withWrapper
+                  />
+                  <Picture
+                    src={Screenshot8}
+                    alt="Función de la app de Recupera"
+                    withWrapper
+                  />
+                  <Picture
+                    src={Screenshot1}
+                    alt="Opciones de la app de Recupera"
+                    withWrapper
+                  />
+                </SecondRowMobile>
+              </animated.div>
+            </Element>
+            <Element className="scroll-element">
+              <animated.div style={card1Animation}>
+                <FirstRowMobile>
+                  <Picture
+                    src={Screenshot6}
+                    alt="Footer de la web de Recupera"
+                    withWrapper
+                  />
+                  <Picture
+                    src={Screenshot9}
+                    alt="Mensaje de éxito de la web de Recupera"
+                    withWrapper
+                  />
+                  <Picture
+                    src={Screenshot5}
+                    alt="Mensaje de error 404 de la web de Recupera"
+                    withWrapper
+                  />
+                </FirstRowMobile>
+              </animated.div>
+            </Element>
+          </PicturesContainerMobile>
+        )}
+      </InView>
     </Fade>
   );
 };
 
 export default ScrollCardAnimation;
 
-const PicturesContainer = styled.div`
+const PicturesContainerDesktop = styled.div`
   display: flex;
   flex-direction: column;
   gap: 14.9px;
   margin: 8% 0 3%;
 
-  img {
-    border-radius: 7.4px;
-    width: 25%;
+  .image {
+    width: 367px;
+    img {
+      border-radius: 7.4px;
+    }
   }
 `;
 
-const FirstRow = styled.div`
+const RowDesktop = styled.div`
   display: flex;
-  flex-direction: row;
   gap: 14.9px;
-  // width: 132%;
-  align-self: center;
-  position: relative;
-  right: 18%;
-`;
-
-const SecondRow = styled.div`
-  display: flex;
-  flex-direction: row;
-  gap: 14.9px;
-  // width: 116%;
-  align-self: center;
 `;
 
 const PicturesContainerMobile = styled.div`
@@ -207,26 +228,14 @@ const PicturesContainerMobile = styled.div`
 
 const FirstRowMobile = styled.div`
   display: flex;
-  flex-direction: row;
   gap: 9.5px;
   width: 120%;
   position: relative;
-  right: 80%;
+  right: 70%;
 `;
 
 const SecondRowMobile = styled.div`
   display: flex;
   gap: 9.5px;
-  flex-direction: row;
   width: 161%;
-  align-self: center;
-`;
-
-const ThirdRowMobile = styled.div`
-  display: flex;
-  flex-direction: row;
-  gap: 9.5px;
-  width: 123%;
-  position: relative;
-  right: 80%;
 `;
