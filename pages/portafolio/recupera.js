d54590b5fb997a9ca4eac04fbfd8cde6e5d63996
Rodrigo Salmeron <rodrigosalmeron@192.168.1.3>
2024-08-10 17:02:10 -0600

    deploy fixed

--- pages/portafolio/recupera.js
+++ pages/portafolio/recupera.js
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useCallback } from "react";
 import { useLocalizedContent } from "utils/useLocalizedContent";
 import ssrLocale from "utils/ssrLocale";
 import styled from "styled-components";
